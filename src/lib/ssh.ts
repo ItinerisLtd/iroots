0f17cba355ab12db672ae1e11fdc74d60103075a
codepuncher <lee.hanbury@gmail.com>
2022-04-25 10:34:40 +0100

    🔨(ssh): `keygen` function, return public and private key content

--- src/lib/ssh.ts
+++ src/lib/ssh.ts
@@ -1,10 +1,13 @@
 import * as execa from 'execa'
 import * as fs from 'fs-extra'
 
-export async function keygen(keyFilePath: string, options?: execa.Options) {
-  execa('ssh-keygen', ['-t', 'ed25519', '-C', 'Trellis deploy', '-f', keyFilePath, '-P', ''], options)
+export async function keygen(keyFilePath: string, {keyName = 'Trellis deploy'}, options?: execa.Options) {
+  await execa('ssh-keygen', ['-t', 'ed25519', '-C', keyName, '-f', keyFilePath, '-P', ''], options)
 
-  return fs.readFileSync(keyFilePath, 'utf8')
+  return {
+    private: fs.readFileSync(keyFilePath, 'utf8'),
+    public: fs.readFileSync(`${keyFilePath}.pub`, 'utf8'),
+  }
 }
 
 export async function keyscan(hosts: string[], options?: execa.Options) {
