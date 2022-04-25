136c2106862f2019fc9c8396eb2adb4627b46342
codepuncher <lee.hanbury@gmail.com>
2022-04-25 10:31:45 +0100

    ✨(gh): add `setDeployKey` function

--- src/lib/gh.ts
+++ src/lib/gh.ts
@@ -3,3 +3,7 @@ import * as execa from 'execa'
 export async function setSecret(key: string, value: string, repo: string, options?: execa.Options) {
   return execa('gh', ['secret', 'set', key, '--body', value, '--repo', repo], options)
 }
+
+export async function setDeployKey(key: string, keyName: string, owner: string, repo: string, options?: execa.Options) {
+  return execa('gh', ['api', `repos/${owner}/${repo}/keys`, '-f', `title=${keyName}`, '-f', `key=${key}`, '-f', 'read_only=true'], options)
+}
