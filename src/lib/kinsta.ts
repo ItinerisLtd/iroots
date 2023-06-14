cf355bfb44d07a95978fc9588ed4721b95e8fdac
codepuncher <lee.hanbury@gmail.com>
2023-06-14 13:51:27 +0100

    feat(kinsta): create site command

--- src/lib/kinsta.ts
+++ src/lib/kinsta.ts
@@ -1,3 +1,5 @@
+import {FlagOutput} from '@oclif/core/lib/interfaces/parser.js'
+
 const apiUrl = 'https://api.kinsta.com/v2'
 
 type KinstaSiteLabel = {
@@ -55,13 +57,20 @@ type KinstaEnvironmentsRequest = {
   }
 }
 
+type KinstaCreateSiteResponse = {
+  // eslint-disable-next-line camelcase
+  operation_id: string
+  message: string
+  status: number
+}
+
+type KinstaCreateSiteRequest = KinstaCreateSiteResponse
+
 async function request<TResponse>(token: string, url: string, options: RequestInit = {}): Promise<TResponse> {
-  Object.assign(options, {
-    method: 'GET',
-    headers: {
-      authorization: `Bearer ${token}`,
-    },
-  })
+  const headers = new Headers(options?.headers)
+  headers.set('authorization', `Bearer ${token}`)
+  options.headers = headers
+  options.method ??= 'GET'
 
   return fetch(`${apiUrl}/${url}`, options)
     .then(resp => resp.json())
@@ -84,4 +93,56 @@ export async function getSiteEnvironments(token: string, siteId: string): Promis
   const response = await request<KinstaEnvironmentsRequest>(token, `sites/${siteId}/environments`)
 
   return response.site.environments
+}
+
+export function getRegions(): string[] {
+  return [
+    'asia-east1',
+    'asia-east2',
+    'asia-northeast1',
+    'asia-northeast2',
+    'asia-northeast3',
+    'asia-south1',
+    'asia-south2',
+    'asia-southeast1',
+    'asia-southeast2',
+    'australia-southeast1',
+    'australia-southeast2',
+    'europe-central2',
+    'europe-north1',
+    'europe-southwest1',
+    'europe-west1',
+    'europe-west2',
+    'europe-west3',
+    'europe-west4',
+    'europe-west6',
+    'europe-west8',
+    'europe-west9',
+    'me-west1',
+    'northamerica-northeast1',
+    'northamerica-northeast2',
+    'southamerica-east1',
+    'southamerica-west1',
+    'us-central1',
+    'us-east1',
+    'us-east4',
+    'us-east5',
+    'us-south1',
+    'us-west1',
+    'us-west2',
+    'us-west3',
+    'us-west4',
+  ]
+}
+
+export async function createSite(token: string, args: FlagOutput): Promise<KinstaCreateSiteResponse> {
+  const response = await request<KinstaCreateSiteRequest>(token, `sites`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(args),
+  })
+
+  return response
 }
