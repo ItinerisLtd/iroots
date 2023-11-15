7238c245f8ea1ef163ce9946fdc3bd375efb91a0
codepuncher <lee.hanbury@gmail.com>
2023-11-15 15:25:52 +0000

    feat(sendgrid): list api keys

---
+++ src/commands/sendgrid/api-keys/list.ts
@@ -0,0 +1,29 @@
+import {Flags} from '@oclif/core'
+import {SendGridCommand} from '../../../lib/commands/sendgrid-command.js'
+import {getAllApiKeys} from '../../../lib/sendgrid.js'
+
+export default class List extends SendGridCommand {
+  static description = 'describe the command here'
+
+  static examples = ['<%= config.bin %> <%= command.id %>']
+
+  static flags = {
+    limit: Flags.integer({
+      description: 'Specifies the number of results to be returned by the API.',
+      default: 0,
+    }),
+  }
+
+  public async run(): Promise<void> {
+    const {flags} = await this.parse(List)
+    const {apiKey, limit} = flags
+
+    const response = await getAllApiKeys(apiKey, limit)
+    if (response.errors) {
+      console.table(response.errors)
+      this.exit(1)
+    }
+
+    console.table(response.result)
+  }
+}
---
+++ src/lib/sendgrid.ts
@@ -0,0 +1,39 @@
+const apiUrl = 'https://api.sendgrid.com/v3'
+
+type SendGridApiKey = {
+  // eslint-disable-next-line camelcase
+  api_key_id: string
+  name: string
+}
+
+type SendGridError = {
+  field: string
+  help: string
+  message: string
+}
+
+type SendGridApiKeysResponse = {
+  result: SendGridApiKey[]
+  errors: SendGridError[]
+}
+
+async function request<TResponse>(token: string, url: string, options: RequestInit = {}): Promise<TResponse> {
+  const headers = new Headers(options?.headers)
+  headers.set('authorization', `Bearer ${token}`)
+  headers.set('Content-Type', 'application/json')
+  options.headers = headers
+  options.method ??= 'GET'
+
+  return fetch(`${apiUrl}/${url}`, options)
+    .then(resp => resp.json())
+    .then(data => data as TResponse)
+}
+
+export async function getAllApiKeys(token: string, limit: number = 0): Promise<SendGridApiKeysResponse> {
+  let endpoint = 'api_keys'
+  if (limit > 0) {
+    endpoint = `${endpoint}?limit=${limit}`
+  }
+
+  return request<SendGridApiKeysResponse>(token, endpoint)
+}
