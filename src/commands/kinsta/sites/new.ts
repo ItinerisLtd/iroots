7ff60773fe0a97fb7c0f5349a7f423bc9e7a3c66
codepuncher <lee.hanbury@gmail.com>
2023-12-13 11:11:17 +0000

    feat(kinsta): wait for operation to complete for site create

--- src/commands/kinsta/sites/new.ts
+++ src/commands/kinsta/sites/new.ts
@@ -1,6 +1,6 @@
-import {Flags} from '@oclif/core'
+import {Flags, ux} from '@oclif/core'
 import {KinstaCommand} from '../../../lib/commands/kinsta-command.js'
-import {createSite, getRegions} from '../../../lib/kinsta.js'
+import {createSite, getOperationStatus, getRegions} from '../../../lib/kinsta.js'
 
 export default class New extends KinstaCommand {
   static description = 'Create a new Kinsta site'
@@ -24,12 +24,35 @@ export default class New extends KinstaCommand {
   public async run(): Promise<void> {
     const {flags} = await this.parse(New)
 
+    ux.action.start('Creating site')
     const response = await createSite(flags.apiKey, flags)
     if (response?.status !== 202) {
       console.log(response)
       this.error(response.message, {exit: 2})
     }
 
-    this.log(`Site created. Check MyKinsta to check progress.`)
+    const {operation_id: operationId} = response
+
+    let operationStatus = null
+    let operationStatusCode = 404
+    let requestsCount = 0
+    while (requestsCount < 10 && operationStatusCode === 404) {
+      // This is to make sure that Kinsta have created the operation for us to query.
+      // If we send the request too soon, it will not be ready to view.
+      // eslint-disable-next-line no-await-in-loop
+      await ux.wait(5000)
+
+      // eslint-disable-next-line no-await-in-loop
+      operationStatus = await getOperationStatus(flags.apiKey, operationId)
+      operationStatusCode = operationStatus.status
+      requestsCount++
+    }
+
+    if (operationStatusCode !== 202 && operationStatus !== null) {
+      this.log(`Operation ID: ${operationId}`)
+      this.error(operationStatus.data.message, {exit: 2})
+    }
+
+    ux.action.stop(`Site created. Operation ID: ${operationId}`)
   }
 }
