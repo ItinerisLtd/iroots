02d8c0a4df7816c570ef2fe86dac8b47b57e8ee9
codepuncher <lee.hanbury@gmail.com>
2024-04-15 12:08:59 +0100

    new slugify function

--- src/lib/misc.ts
+++ src/lib/misc.ts
@@ -9,3 +9,20 @@ export function findLastMatch(string: string, pattern: RegExp): string | null {
 
   return lastMatch ? lastMatch[0] : null
 }
+
+/**
+ * @url https://byby.dev/js-slugify-string
+ *
+ * @param string the string to slugify
+ * @returns string the slugified string
+ */
+export function slugify(string: string): string {
+  return String(string)
+    .normalize('NFKD') // split accented characters into their base characters and diacritical marks
+    .replace(/[\u0300-\u036F]/g, '') // remove all the accents, which happen to be all in the \u03xx UNICODE block.
+    .trim() // trim leading or trailing whitespace
+    .toLowerCase() // convert to lowercase
+    .replace(/[^\d a-z-]/g, '') // remove non-alphanumeric characters
+    .replace(/\s+/g, '-') // replace spaces with hyphens
+    .replace(/-+/g, '-') // remove consecutive hyphens
+}
